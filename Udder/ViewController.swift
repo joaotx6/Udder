0204dba68e085db6e7661c6e131506af50c956c5
Joao Teixeira <92153307+joaotx6@users.noreply.github.com>
2022-11-01 10:21:00 +0000

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Joao Teixeira on 01/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
